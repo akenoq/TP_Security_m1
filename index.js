143c41dabd369e6181861cc1611206f68f4d08b1
akenoq <akenoq@yandex.ru>
2018-03-12 19:16:48 +0300

    add rss

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/xxe', function(req, res) {
     res.sendfile("static/xxe.xml");
 });
 
+app.get('/rss', function(req, res) {
+    res.sendfile("static/rss.rss");
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 console.log("Server works on port " + port);
